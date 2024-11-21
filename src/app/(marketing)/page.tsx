289d5e44dec6eb4c1d3ed4122b6fea86dab97b7a
Kristjan Lääne <xonproject@gmail.com>
2024-11-21 14:15:13 +0200

    pricing section

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,17 +1,21 @@
 import { Button } from '@/components/ui/button';
 import { SignUpButton } from '@clerk/nextjs';
-import { ArrowRightIcon } from 'lucide-react';
+import { ArrowRightIcon, CheckIcon } from 'lucide-react';
 import Link from 'next/link';
 import { NeonIcon } from './_icons/NeonIcon';
 import { ClerkIcon } from './_icons/ClerkIcon';
 import { subscriptionTiersInOrder } from '@/data/subscriptionTiers';
 import {
   Card,
+  CardContent,
   CardDescription,
+  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
 import { formatCompactNumber } from '@/lib/formatters';
+import { ReactNode } from 'react';
+import { cn } from '@/lib/utils';
 
 export default function HomePage() {
   return (
@@ -92,14 +96,47 @@ function PricingCard({
   canAccessAnalytics,
   canCustomizeBanner,
 }: (typeof subscriptionTiersInOrder)[number]) {
+  const isMostPopular = name === 'Standard';
+
   return (
     <Card>
       <CardHeader>
-        <CardTitle>${priceInCents / 100} /mo</CardTitle>
+        <div className='text-accent font-semibold mb-8'>{name}</div>
+        <CardTitle className='text-xl font-bold'>
+          ${priceInCents / 100} /mo
+        </CardTitle>
         <CardDescription>
           {formatCompactNumber(maxNumberOfVisits)} pricing page visits/mo
         </CardDescription>
       </CardHeader>
+      <CardContent>
+        <SignUpButton>
+          <Button
+            className='text-lg w-full rounded-lg'
+            variant={isMostPopular ? 'accent' : 'default'}
+          >
+            Get Started
+          </Button>
+        </SignUpButton>
+      </CardContent>
+      <CardFooter>
+        <Feature>ss</Feature>
+      </CardFooter>
     </Card>
+  );
+}
+
+function Feature({
+  children,
+  className,
+}: {
+  children: ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cn('flex items-center gap-2', className)}>
+      <CheckIcon className='size-4 stroke-accent bg-accent/25 rounded-full p-0.5' />
+      <span>{children}</span>
+    </div>
   );
 }
